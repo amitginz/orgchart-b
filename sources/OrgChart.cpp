99f1769b79a969fd04bbd2ab6d3761600bb51d6a
amitginz <74299421+amitginz@users.noreply.github.com>
2022-05-25 12:40:00 +0300

    final commit

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -15,14 +15,14 @@ using namespace std;
 
     }
     //add the head of chart
-    OrgChart& OrgChart::add_root(const string root){
+    OrgChart& OrgChart::add_root(const string& root){
         Node temp;
         temp.data = root;
         temp.father = "";
         temp.left = "";
         temp.middle = "";
         temp.right = "";
-        if(this->chart.size() == 0){
+        if(this->chart.empty()){
             this->chart.push_back(temp.data);
             this->chart1.push_back(temp);
         }
@@ -38,7 +38,7 @@ using namespace std;
     }
 
     //add sub to the head of chart
-    OrgChart& OrgChart::add_sub(const string root, const string sub){
+    OrgChart& OrgChart::add_sub(const string& root, const string& sub){
         Node sun;
         sun.data = sub;
         sun.father = root;
@@ -47,13 +47,13 @@ using namespace std;
             string tmp = this->chart1.at(i).data;
             if (tmp == root){
                 indeed = true;
-                if(this->chart1.at(i).left == ""){
+                if(this->chart1.at(i).left.empty()){
                     this->chart1.at(i).left = sub;
                 }
-                else if(this->chart1.at(i).middle == ""){
+                else if(this->chart1.at(i).middle.empty()){
                     this->chart1.at(i).middle = sub;
                 }
-                else if(this->chart1.at(i).right == ""){
+                else if(this->chart1.at(i).right.empty()){
                     this->chart1.at(i).right = sub;
                 }
                 
@@ -62,7 +62,7 @@ using namespace std;
         if(!indeed){
             throw runtime_error("can't add sub to non exist root...");
         }
-        if(this->chart.size() == 0){
+        if(this->chart.empty()){
             throw runtime_error("this chart is empty");
         }
         this->chart.push_back(sun.data);
@@ -82,23 +82,27 @@ using namespace std;
     }
 
     vector<string>::iterator OrgChart::begin_level_order(){
-        uint place = 0;
-        int k = 0;
+        set_place();
         vector<string> temp;
-        vector<int> brothers;
-        temp.push_back(this->chart.at(0));
-        brothers.push_back(0);
-        for(uint i = 0; i<this->chart.size();i++){
-            for(uint j = i+1; j<this->chart.size();j++){
-                string root = this->chart1.at(i).data;
-                if(this->chart1.at(j).father == root){
-                    k++;
-                    temp.push_back(this->chart1.at(j).data);
-                }
+        bool same = true;
+        for(uint i = 1; i<this->chart1.size();i++){
+            if(this->chart1.at(i).data != this->chart1.at(i-1).data){
+                same = false;
             }
         }
+        if(!same){
+            temp.push_back(this->chart.at(0));
+            for(uint i = 0; i<this->chart.size();i++){
+                for(uint j = i+1; j<this->chart.size();j++){
+                    string root = this->chart1.at(i).data;
+                    if(this->chart1.at(j).father == root){
+                        temp.push_back(this->chart1.at(j).data);
+                    }
+                }
+            }
+            this->chart = temp;
+        }
         
-        this->chart = temp;
         this->it = this->chart.begin();
         if(this->chart.empty()){
             throw runtime_error("chart is empty..");
@@ -114,36 +118,45 @@ using namespace std;
 
      //iterate reverse level order on the chart
     vector<string>::iterator OrgChart::begin_reverse_order(){
-        uint place = 0;
+        set_place();
         stack<string> stack;
         queue<string> que;
         vector<string> temp;
-        que.push(this->chart.at(0));
-        while(!que.empty()){
-            string front = que.front();
-            que.pop();
-            stack.push(front);
-            for(uint j = 0; j<this->chart1.size();j++){
-                if(this->chart1.at(j).data == front){
-                    if(this->chart1.at(j).right != ""){
-                        que.push(this->chart1.at(j).right);
-                    }
-                    if(this->chart1.at(j).middle != ""){
-                        que.push(this->chart1.at(j).middle);
-                    }
-                    if(this->chart1.at(j).left != ""){
-                        que.push(this->chart1.at(j).left);
-                    }
+        bool same = true;
+        for(uint i = 1; i<this->chart1.size();i++){
+            if(this->chart1.at(i).data != this->chart1.at(i-1).data){
+                same = false;
+            }
+        }
+        if(!same){
+            que.push(this->chart.at(0));
+            while(!que.empty()){
+                string front = que.front();
+                que.pop();
+                stack.push(front);
+                for(uint j = 0; j<this->chart1.size();j++){
+                    if(this->chart1.at(j).data == front){
+                        if(!this->chart1.at(j).right.empty()){
+                            que.push(this->chart1.at(j).right);
+                        }
+                        if(!this->chart1.at(j).middle.empty()){
+                            que.push(this->chart1.at(j).middle);
+                        }  
+                        if(!this->chart1.at(j).left.empty()){
+                            que.push(this->chart1.at(j).left);
+                        }
                 }
                 
             }
         }
-        while(!stack.empty()){
-            string top = stack.top();
-            stack.pop();
-            temp.push_back(top);
+            while(!stack.empty()){
+                string top = stack.top();
+                stack.pop();
+                temp.push_back(top);
+            }
+            this->chart = temp;
         }
-        this->chart = temp;
+        
         
         this->it = this->chart.begin();
         if(this->chart.empty()){
@@ -162,30 +175,39 @@ using namespace std;
 
     //iterate pre order on the chart
     vector<string>::iterator OrgChart::begin_preorder(){
-        uint place = 0;
+        set_place();
         vector<string> temp;
         stack<string> stack;
-        stack.push(this->chart1.at(0).data);
-        while(!stack.empty()){
-            string root = stack.top();
-            stack.pop();
-            temp.push_back(root);
-            for(uint j = 0; j<this->chart1.size();j++){
-                if(this->chart1.at(j).data == root){
-                    if(this->chart1.at(j).right != ""){
-                        stack.push(this->chart1.at(j).right);
-                    }
-                    if(this->chart1.at(j).middle != ""){
-                        stack.push(this->chart1.at(j).middle);
-                    }
-                    if(this->chart1.at(j).left != ""){
-                        stack.push(this->chart1.at(j).left);
-                    }
-                }
-                
+        bool same = true;
+        for(uint i = 1; i<this->chart1.size();i++){
+            if(this->chart1.at(i).data != this->chart1.at(i-1).data){
+                same = false;
             }
         }
-        this->chart = temp;
+        if(!same){
+            stack.push(this->chart1.at(0).data);
+            while(!stack.empty()){
+                string root = stack.top();
+                stack.pop();
+                temp.push_back(root);
+                for(uint j = 0; j<this->chart1.size();j++){
+                    if(this->chart1.at(j).data == root){
+                        if(!this->chart1.at(j).right.empty()){
+                            stack.push(this->chart1.at(j).right);
+                        }
+                        if(!this->chart1.at(j).middle.empty()){
+                            stack.push(this->chart1.at(j).middle);
+                        }
+                        if(!this->chart1.at(j).left.empty()){
+                            stack.push(this->chart1.at(j).left);
+                        }
+                    }
+                    
+                }
+            }
+            this->chart = temp;
+        }
+        
         this->it=this->chart.begin();
         if(this->chart.empty()){
             throw runtime_error("chart is empty..");
@@ -230,29 +252,28 @@ using namespace std;
         return *this->it;
     }
     int OrgChart::size(){
-        return this->chart.at(place++).size();
+        return this->chart.at(this->place++).size();
     }
 
     int OrgChart::length(){
-        return this->chart.at(place++).size();
+        return this->chart.at(this->place++).size();
     }
 
     bool operator!=(const OrgChart& lhs,const string& rhs){
-
-        // return (*this != comp);
         return false;
     }
 
-    bool operator==(const OrgChart& lhs, const string& rhs){
+    bool operator==(OrgChart& lhs, string& rhs){
         return true;
     }
 
     bool OrgChart::operator!=(const OrgChart& rhs){
-
-        // return (*this != comp);
         return this->it != rhs.it;
     }
 
     bool OrgChart::operator==(const OrgChart& rhs){
         return this->it == rhs.it;
+    }
+    void OrgChart::set_place(){
+        this->place = 0;
     }
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -1,5 +1,3 @@
-// #ifndef OrgChart_HPP
-// #define OrgChart_HPP
 #include <iostream>
 #include <ostream>
 #include <string>
@@ -13,16 +11,20 @@ namespace ariel{
         vector<string> chart;
         vector<Node> chart1;
         vector<string>::iterator it;
-    public:
         uint place = 0;
+    public:
         OrgChart(){
 
         }
+        OrgChart(const OrgChart&) =delete;
+        OrgChart& operator =(const OrgChart&)=delete;
+        OrgChart(OrgChart&&) = delete;
+        OrgChart& operator=(OrgChart&&) = delete;
         ~OrgChart();
         int size();
         int length();
-        OrgChart& add_root(const string root);
-        OrgChart& add_sub(const string root, const string sub);
+        OrgChart& add_root(const string& root);
+        OrgChart& add_sub(const string& root, const string& sub);
         vector<string>::iterator begin();
         vector<string>::iterator end();
         vector<string>::iterator begin_level_order();
@@ -34,14 +36,14 @@ namespace ariel{
         vector<string>::iterator operator++();
         vector<string>::iterator operator++(int);
         string operator*();
-        friend bool operator!=(const OrgChart lhs,const string& rhs);
-        friend bool operator==(const OrgChart lhs, const string rhs);
-        bool operator!=(const OrgChart& lhs);
-        bool operator==(const OrgChart& lhs);
+        friend bool operator!=(OrgChart lhs,string& rhs);
+        friend bool operator==(OrgChart lhs,string& rhs);
+        bool operator!=(const OrgChart& rhs);
+        bool operator==(const OrgChart& rhs);
         string* operator->();
+        void set_place();
         friend ostream& operator<< (ostream& output, const OrgChart& chart);
         
     };
 
 }
-// #endif
